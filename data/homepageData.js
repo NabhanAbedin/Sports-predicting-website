b556c7f49dbd4e0702288ce8c9582b7583509379
NabhanAbedin <nabedi2006@gmail.com>
2025-01-06 02:48:56 -0500

    changed the html cards to be generated through js, also created a trivia page html

---
+++ data/homepageData.js
@@ -0,0 +1,16 @@
+export const homepageItems = [{
+    image: "images/trivia",
+    header: "Sports Trivia!",
+    text: "Challenge yourself by answering trivia questions about your favorite teams and players! Test your knowledge on historical moments, player stats, memorable matches, and iconic achievements while discovering fascinating facts about the game you love!",
+    link: 'triviaPage.html'
+}, {
+    image: "images/nba stadium",
+    header: "NBA Predictor",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi saepe repellat sit! Aliquid harum expedita, dolorum iste doloribus a! Similique commodi fuga pariatur exercitationem officiis  nulla possimus dolor quam.",
+    link: 'none'
+}, {
+    image: "images/soccerStadium",
+    header: "European Football Predictor",
+    text: "CLorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi saepe repellat sit! Aliquid harum expedita, dolorum iste doloribus a! Similique commodi fuga pariatur exercitationem officiis  nulla possimus dolor quam.",
+    link: 'none'
+}];
